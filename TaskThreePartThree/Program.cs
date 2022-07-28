fdb12920a833dea324661dd05dc3232f7ee5e724
solfire1994 <solfire1994@gmail.com>
2022-07-28 12:06:44 +0300

    Add project files.

---
+++ TaskThreePartThree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaskThreePartThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число и программа проверит простое ли оно");
+            int number = int.Parse(Console.ReadLine());
+            int i = 2;
+            bool isValid = false;
+            while (i < number)
+            {
+                if(number % i == 0)
+                {
+                    isValid = true;
+                }
+                i++;
+            }
+
+            Console.WriteLine(isValid ? "Число не простое" : "Число простое");
+            Console.ReadKey();
+        }
+    }
+}
